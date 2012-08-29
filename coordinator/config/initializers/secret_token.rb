258d606899581488e784d0af5118553c27790596
an3sarmiento <a.sarmiento64@uniandes.edu.co>
2012-08-28 17:12:43 -0700

    Initial commit

---
+++ coordinator/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Coordinator::Application.config.secret_token = 'abf3ea89417f9367f2e6a46f3fcdfa73cefc71155926f9bc14d6af98a640db76e129f4b0e4c9258edd56db641f050b3f49828abdbce7ba80a6d17b9e30760ec7'
