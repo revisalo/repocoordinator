957656922ccdb3925b986f68400a58a112436cf8
Laura Lunita Lopez <lunicirus@gmail.com>
2012-09-08 11:14:28 -0500

    creacion de materia
    cree el scaffold de materia y lo migre a la base de datos

---
+++ coordinator/app/models/materium.rb
@@ -0,0 +1,3 @@
+class Materium < ActiveRecord::Base
+  attr_accessible :codigo, :nombre
+end
--- coordinator/config/routes.rb
+++ coordinator/config/routes.rb
@@ -1,4 +1,6 @@
 Coordinator::Application.routes.draw do
+  resources :materia
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
