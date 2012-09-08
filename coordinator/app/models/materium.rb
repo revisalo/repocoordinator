c09495aed96a87cd520d44b4ce5d253548692367
Laura Lunita Lopez <lunicirus@gmail.com>
2012-09-08 11:38:23 -0500

    materia pertenece a pensum
    aºadi el belongs_to

--- coordinator/app/models/materium.rb
+++ coordinator/app/models/materium.rb
@@ -1,3 +1,4 @@
 class Materium < ActiveRecord::Base
   attr_accessible :codigo, :nombre
+  belongs_to :pensums
 end
--- coordinator/db/schema.rb
+++ coordinator/db/schema.rb
@@ -11,6 +11,13 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 0) do
+ActiveRecord::Schema.define(:version => 20120908161052) do
+
+  create_table "materia", :force => true do |t|
+    t.string   "nombre"
+    t.string   "codigo"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
 end
